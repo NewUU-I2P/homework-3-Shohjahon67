ccb77d46343d82e27dda04a1ebb2ec6449c5d2db
Shohjakhon Jonuzokov <sh.jonuzoqov@newuu.uz>
2023-11-11 17:02:42 +0500

    1

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -6,9 +6,9 @@ std::string problemSolution4(const std::string &macAddress) {
     std::string result;
     if (macAddress == "FF:FF:FF:FF:FF:FF"){
         result = "Broadcast";
-    } else if ((macAddress[0] + macAddress[1]) % 2 == 0){
+    } else if (macAddress[1] % 2 == 0){
         result = "Unicast";
-    } else if ((macAddress[0] + macAddress[1]) % 2 != 0){
+    } else if (macAddress[1] % 2 != 0){
         result = "Multicast";
     }
 
